48c025fbc9fda93623c9208eb8fc822adee71a6d
Artyom <artem.stepanenko.kit@gmail.com>
2024-10-09 16:50:41 +0300

    Database, csv example

--- Application/Services/ContactService/UploadCsvFile.cs
+++ Application/Services/ContactService/UploadCsvFile.cs
@@ -16,41 +16,41 @@ namespace Application.Services.ContactService
             _context = context;
         }
 
-       public async Task<int> AddContactsFromCsvAsync(IFormFile file)
-{
-    var contacts = new List<Contact>();
-
-    using (var stream = new StreamReader(file.OpenReadStream()))
-    {
-        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                HeaderValidated = null, // Disable header validation
-                MissingFieldFound = null // Ignore missing fields
-            };
-        using (var csv = new CsvReader(stream, config))
+        public async Task<int> AddContactsFromCsvAsync(IFormFile file)
         {
-            var records = csv.GetRecords<Contact>().ToList();
-            foreach (var record in records)
+            var contacts = new List<Contact>();
+
+            using (var stream = new StreamReader(file.OpenReadStream()))
             {
-                var contact = new Contact
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
                 {
-                    Id = Guid.NewGuid(),
-                    Name = record.Name,
-                    DateOfBirth = record.DateOfBirth,
-                    Married = record.Married,
-                    Phone = record.Phone,
-                    Salary = record.Salary
+                    HeaderValidated = null,
+                    MissingFieldFound = null
                 };
-                contacts.Add(contact);
+                using (var csv = new CsvReader(stream, config))
+                {
+                    var records = csv.GetRecords<Contact>().ToList();
+                    foreach (var record in records)
+                    {
+                        var contact = new Contact
+                        {
+                            Id = Guid.NewGuid(),
+                            Name = record.Name,
+                            DateOfBirth = record.DateOfBirth,
+                            Married = record.Married,
+                            Phone = record.Phone,
+                            Salary = record.Salary
+                        };
+                        contacts.Add(contact);
+                    }
+                }
             }
+
+            await _context.Contacts.AddRangeAsync(contacts);
+            await _context.SaveChangesAsync();
+
+            return contacts.Count;
         }
-    }
-
-    await _context.Contacts.AddRangeAsync(contacts);
-    await _context.SaveChangesAsync();
-
-    return contacts.Count;
-}
 
     }
 }
